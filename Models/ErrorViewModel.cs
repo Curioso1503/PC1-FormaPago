ecf5a7ec628af100a0ee196807dae5b5fe411ac1
Curioso1503 <estoestodo7@gmail.com>
2023-03-24 23:18:34 -0500

    PC1-LopezLimay,JorgeArmando

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PC1_FormaPago.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
